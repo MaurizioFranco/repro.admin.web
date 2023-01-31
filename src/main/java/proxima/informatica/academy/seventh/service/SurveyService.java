96cd869548be1296872b2263feab95ea283f2501
Academy3 <Academy3@192.168.56.1>
2023-01-31 14:57:40 +0100

    Commit SurveyService moved

---
+++ src/main/java/proxima/informatica/academy/seventh/service/SurveyService.java
@@ -0,0 +1,56 @@
+package proxima.informatica.academy.seventh.service;
+
+
+import centauri.academy.proxima.cerepro.entity.Surveys;
+import centauri.academy.proxima.cerepro.repository.SurveysRepository;
+
+public class SurveyService {
+
+	SurveysRepository surveyRepository = null ;
+	
+	private SurveyService() {
+		surveyRepository = new SurveysRepository();
+	}
+	
+	private static SurveyService instance;
+	
+	public static SurveyService getInstance() {
+		if(instance == null) {
+			instance = new SurveyService();
+		}
+		return instance;
+	}
+	
+	public boolean insert(Surveys survey) {
+		boolean response = false;
+		
+		if (surveyRepository.create(survey) > 0) {
+			response = true;
+		}
+		return response;
+	}
+	
+//	public List<Surveys> findAllSurveys() {
+//		List<Surveys> listSurveys = new ArrayList<Surveys>();
+//		listSurveys = surveyRepository.findAll();
+//		return listSurveys;
+//	}
+	
+	public boolean deleteSurvey(int id) {
+		boolean response = false;
+		if (surveyRepository.delete(id)) {
+			response = true;
+		}
+		return response;
+	}
+	
+	public Surveys selectById(int id) {
+		Surveys surveyRetrieved = new Surveys();
+		surveyRetrieved = (Surveys)surveyRepository.findById((long)id);
+		return surveyRetrieved;
+	}
+	
+	public boolean updateSurvey(Surveys survey) {
+		return surveyRepository.update(survey);
+	}
+}
