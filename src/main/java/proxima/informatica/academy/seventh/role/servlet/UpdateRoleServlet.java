ca669da39bc86b8f8945ff8b733ad3289c4c595f
MaurizioFranco <maurizio.franco@ymail.com>
2023-01-31 11:01:27 +0100

    added jquery support

--- src/main/java/proxima/informatica/academy/seventh/role/servlet/UpdateRoleServlet.java
+++ src/main/java/proxima/informatica/academy/seventh/role/servlet/UpdateRoleServlet.java
@@ -28,11 +28,34 @@ public class UpdateRoleServlet extends HttpServlet {
         super();
     }
 
+    /**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		logger.debug("UpdateRoleServlet.doGet START - " + request.getQueryString());
+		logger.debug("UpdateRoleServlet.doGet DEBUG - id: " + request.getParameter("id"));
+		RoleDto role = new RoleDto();
+		role.setId(Integer.parseInt(request.getParameter("id")));
+		role.setLabel(request.getParameter("label"));
+		role.setDescription(request.getParameter("description"));
+		role.setLevel(Integer.parseInt(request.getParameter("level")));
+
+		boolean responseValue = RoleService.getInstance().updateRole(role) ;
+        logger.debug("InsertRoleServlet.DEBUG - responseValue: " + responseValue);
+		if (responseValue) {
+			response.getWriter().append("OK");
+		} else {
+			response.getWriter().append("KO");
+		}
+	}
+    
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		logger.debug("UpdateRoleServlet.START - " + request.getQueryString());
+		logger.debug("UpdateRoleServlet.DEBUG - id: " + request.getParameter("id"));
+		logger.debug("UpdateRoleServlet.DEBUG - id: " + request.getAttribute("id"));
 		RoleDto role = new RoleDto();
 		role.setId(Integer.parseInt(request.getParameter("id")));
 		role.setLabel(request.getParameter("label"));
